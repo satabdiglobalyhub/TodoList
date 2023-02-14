d9b50556e178660653afd750d1efce505ebecc7c
satabdiglobalyhub <satabdi.gautam@globalyhub.com>
2023-02-14 17:27:17 +0545

    feat:list delete

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,65 @@
 <template>
-  <h1 class="heading">TODO LIST</h1>
-  
+  <form @submit.prevent="addNewTodo">
+    <label>todos</label>
+    <input
+      type="text"
+      name="newTodo"
+      v-model="newTodo"
+      placeholder="Enter Task"
+    />
+    <button>Submit</button>
+  </form>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="todo.id" class="todo">
+      <h3 :class="{ done: todo.done }" @click="toggleDone(todo)">
+        {{ todo.content }}
+        <button @click="removeTodo(index)">Delete</button>
+      </h3>
+    </li>
+  </ul>
 </template>
 
-<script></script>
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const newTodo = ref("");
+    const todos = ref([]);
+
+    function addNewTodo() {
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+      newTodo.value = "";
+    }
+
+    function toggleDone(todo) {
+      todo.done = !todo.done;
+    }
+
+    function removeTodo(index) {
+      todos.value.splice(index, 1);
+    }
+
+    return {
+      newTodo,
+      todos,
+      addNewTodo,
+      toggleDone,
+      removeTodo,
+    };
+  },
+};
+</script>
 
 <style>
-.heading {
-  display: flex;
-  justify-content: center;
-  margin-top: 50px;
-  color: rgb(35, 29, 29);
+.todo {
+  cursor: pointer;
+}
+.done {
+  text-decoration: line-through;
 }
 </style>
