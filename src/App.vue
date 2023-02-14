4e4a6a852eda3b845eba09a35eab25f0f23b8885
satabdiglobalyhub <satabdi.gautam@globalyhub.com>
2023-02-14 11:04:55 +0545

    feat:heading

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
-<template>lists</template>
+<template>
+  <h1 class="heading">TODO LIST</h1>
+  
+</template>
 
 <script></script>
 
-<style></style>
+<style>
+.heading {
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  color: rgb(35, 29, 29);
+}
+</style>
